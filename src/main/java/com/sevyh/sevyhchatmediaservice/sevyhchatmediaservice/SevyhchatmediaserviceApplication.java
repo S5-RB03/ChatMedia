e9ea234282e28d94a9ff50c4a700810ebe0a0f48
Jules <jules.houben@live.nl>
2023-04-04 13:48:42 +0200

    Install springboot maven project

---
+++ src/main/java/com/sevyh/sevyhchatmediaservice/sevyhchatmediaservice/SevyhchatmediaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sevyh.sevyhchatmediaservice.sevyhchatmediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevyhchatmediaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevyhchatmediaserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevyh/sevyhchatmediaservice/sevyhchatmediaservice/SevyhchatmediaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevyh.sevyhchatmediaservice.sevyhchatmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevyhchatmediaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

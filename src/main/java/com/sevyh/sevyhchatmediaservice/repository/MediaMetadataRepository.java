4bcf32023c84c9ddd7eb107483b9159b8e77ca64
Jules <jules.houben@live.nl>
2023-04-05 13:28:27 +0200

    Remove unecessary import of Repository

--- src/main/java/com/sevyh/sevyhchatmediaservice/repository/MediaMetadataRepository.java
+++ src/main/java/com/sevyh/sevyhchatmediaservice/repository/MediaMetadataRepository.java
@@ -2,10 +2,8 @@ package com.sevyh.sevyhchatmediaservice.repository;
 
 import com.sevyh.sevyhchatmediaservice.api.model.MediaMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface MediaMetadataRepository extends JpaRepository<MediaMetadata, UUID> {
 }
